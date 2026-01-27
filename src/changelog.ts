07be97855cd921183672491c8f54e16191cfd4a8
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2026-01-27 19:14:19 +0900

    changelogを構造化

---
+++ src/changelog.ts
@@ -0,0 +1,23 @@
+export type ChangeLog = {
+  version: string;
+  date: string;
+  body: string | string[];
+};
+
+export const changeLog = [
+  {
+    version: "1.2.0",
+    date: "2026/01/27",
+    body: ["アップロードのUIを改良し、zipインポートと統合"],
+  },
+  {
+    version: "1.1.0",
+    date: "2025/12/10",
+    body: "zipインポート機能を追加",
+  },
+  {
+    version: "1.0.0",
+    date: "2024/9/2",
+    body: "初回リリース",
+  },
+];
